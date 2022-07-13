8e75c7da7323e28e87e5533f7b4c0a2a51d9258f
alexandre <alexandre@pop-os.localdomain>
2022-07-13 15:14:57 -0300

    script de instalação automatica

---
+++ README.md
@@ -0,0 +1,7 @@
+Script de instalação automática,irá baixar os seguintes arquivos:
+-Visual Studio Code
+-Google Chrome
+-Spotify
+-Cpu-x
+-Git
+-Python3 e o gerenciador de pacotes Pip
---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Script para fazer download de arquivos automatico
+
+sudo apt-get update;
+
+sudo apt-get upgrade;
+
+sudo apt install code;
+
+sudo apt-get install google-chrome-stable;
+
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -;
+
+sudo apt install cpu-x;
+
+sudo apt install python3-pip;
+
+sudo apt-get install git-all;
+
+sudo apt install gdebi-core wget;
